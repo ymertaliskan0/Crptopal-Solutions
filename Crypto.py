775c110f41a2c48f55d5eba6feecf673dea1212e
Ymertaliskan <Y.M.Alskan@student.tudelft.nl>
2026-03-02 22:18:11 +0100

    Solution for Question 5

--- Crypto.py
+++ Crypto.py
@@ -56,4 +56,19 @@ def findLine(path = "4.txt"):
 
 
 
-# Question 5 
+# Question 5 repeating-key XOR
+def repeatXOR(s, pattern = b"ICE"):
+    index = 0
+    result = bytearray()
+    for c in s:
+        xor = pattern[index] ^ c
+        index = (index + 1) % len(pattern)
+        result.append(xor)
+    return bytes(result)
+
+text = (b"Burning 'em, if you ain't quick and nimble\n"
+        b"I go crazy when I hear a cymbal")
+
+res = "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f"
+# assert repeatXOR(text).hex() == res, "Assertion failed: The output does not match!"
+print(repeatXOR(text).hex())
